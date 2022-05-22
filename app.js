d4edeae80bac7b7fe7abf1f6332e285514696abc
sonia <sonia1.kim@cheilpengtai.com>
2022-05-22 20:38:30 +0900

    d

---
+++ app.js
@@ -0,0 +1,55 @@
+/* 
+
+//array
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// Get Item from Array
+console.log(daysOfWeek);
+
+// Add one more day to the Array
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
+
+
+
+const toBuy = ["potato", "tomato", "pizza"];
+toBuy.push("kimbab");
+
+console.log(toBuy[100]); //undefined
+
+*/
+
+
+/*
+
+// object
+const player = {
+    name: "sonia", // " name: 'sonia' "는 property
+    points: 10,
+    fat: true,
+};
+
+console.log(player);
+console.log(player["name"]);
+console.log(player.name);
+// console is an object and that somewhere has a 'log'
+
+player.fat = false; //const 안의 프로퍼티는 업데이트 가능
+
+player.lastName = "potate"; //프로퍼티 추가도 가능
+console.log(player);
+
+player.points = player.points + 15;
+console.log(player.points);
+
+*/
+
+
+console.log("Hello my name is Nico");
+console.log("Hello my name is Dal");
+console.log("Hello my name is Shigatsu");
+console.log("Hello my name is Nico");
+console.log("Hello my name is Nico");
+
+
