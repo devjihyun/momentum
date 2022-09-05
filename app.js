c9b19b9aeaa8a1be1d49539ee4c621089206c13a
sonia <sonia1.kim@cheilpengtai.com>
2022-09-05 15:48:13 +0900

    스터디 업뎃

--- app.js
+++ app.js
@@ -46,10 +46,76 @@ console.log(player.points);
 */
 
 
-console.log("Hello my name is Nico");
-console.log("Hello my name is Dal");
-console.log("Hello my name is Shigatsu");
-console.log("Hello my name is Nico");
-console.log("Hello my name is Nico");
+/*
+const calculator = {
+    plus: function(a, b) {
+        console.log(a + b);
+    },
+    minus: function(a, b) {
+        console.log(a - b);
+    },
+    multiply: function(a, b) {
+        console.log(a * b);
+    },
+    divide: function(a, b) {
+        console.log(a / b);
+    },
+    power: function(a, b) {
+        console.log(a ** b);
+    }
+};
+
+calculator.plus(2, 7);
+calculator.minus(3, 4);
+calculator.multiply(5, 1);
+calculator.divide(5, 10);
+calculator.power(2, 5);
+
+*/
+
+/*
+const calculator = {
+    plus: function(a, b) {
+        alert(a + b);
+    },
+    minus: function(a, b) {
+        alert(a - b);
+    },
+    multiply: function(a, b) {
+        alert(a * b);
+    },
+    divide: function(a, b) {
+        alert(a / b);
+    },
+    power: function(a, b) {
+        alert(a ** b);
+    }
+};
+
+console.log(calculator.plus(2, 3));
+
+*/
 
 
+const calculator = {
+    plus: function(a, b) {
+        return a + b;
+    },
+    minus: function(a, b) {
+        return a - b;
+    },
+    multiply: function(a, b) {
+        return a * b;
+    },
+    divide: function(a, b) {
+        return a / b;
+    },
+    power: function(a, b) {
+        return a ** b;
+    }
+};
+const plusResult = calculator.plus(2, 3); //5
+const minusResult = calculator.minus(plusResult, 10); //-5
+const multiplyResult = calculator.multiply(10, minusResult); //-50
+const divideResult = calculator.divide(multiplyResult, plusResult); //-10
+const powerResult = calculator.power(divideResult, minusResult); //0.000009999999999999999
