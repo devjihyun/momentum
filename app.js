bf6caddfb981de2364211bccd5db74bd3b26142b
sonia <sonia1.kim@cheilpengtai.com>
2022-11-01 19:34:10 +0900

    스터디 정리

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const title = document.querySelector(".hello h1");
-
-console.log(title);
-
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
 
-// - getElementsByClassName() : 많은 element를 가져올때 씀(array를 반환)
-// - getElementsByTagName() : name을 할당할 수 있음(array를 반환)
-// - querySelector : element를 CSS selector방식으로 검색할 수 있음 (ex. h1:first-child)
+function onLoginBtnClick() {
+    const username = loginInput.value;
+    console.log(username);
+}
+
+loginButton.addEventListener("click", onLoginBtnClick);
